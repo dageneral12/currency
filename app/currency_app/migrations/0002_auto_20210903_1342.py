9cfeb233373d53fce9ec15991ab6241d15e2f5d6
dageneral12 <dageneral@gmail.com>
2021-09-04 18:24:41 +0300

    Update 0002_auto_20210903_1342.py

--- app/currency_app/migrations/0002_auto_20210903_1342.py
+++ app/currency_app/migrations/0002_auto_20210903_1342.py
@@ -29,7 +29,8 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='contactus',
             name='created',
-            field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now),
+            field=models.DateTimeField(auto_now_add=True, 
+                                       default=django.utils.timezone.now),
             preserve_default=False,
         ),
         migrations.AlterField(
